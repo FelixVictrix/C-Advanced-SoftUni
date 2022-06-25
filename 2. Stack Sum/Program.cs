9a1de4d15c44269f96f3a9514046f93e60eb5775
FelixVictrix <93392968+FelixVictrix@users.noreply.github.com>
2022-06-25 21:31:08 +0300

    0

--- 2. Stack Sum/Program.cs
+++ 2. Stack Sum/Program.cs
@@ -8,13 +8,56 @@ namespace _2._Stack_Sum
     {
         static void Main(string[] args)
         {
-            
-            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
 
-          Stack<int> stack = new Stack<int>(input);
+            int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
 
-            string 
+            Stack<int> stack = new Stack<int>(array);
 
+            string input = Console.ReadLine().ToLower();
+
+            while (input != "end")
+            {
+                var current = input.Split();
+                var command = current[0];
+
+                switch (command)
+                {
+                    case "add":
+                        AddStack(stack, current);
+                        break;
+                    case "remove":
+                        RemoveStack(stack, current);
+                        break;
+                }
+                input = Console.ReadLine().ToLower();
+            }
+            if (stack.Any())
+            {
+                int sum = stack.Sum();
+                Console.WriteLine($"Sum: {sum}");
+            }
+
+        }
+
+        static void RemoveStack(Stack<int> stack, string[] current)
+        {
+            int remove = int.Parse(current[1]);
+            if (stack.Count >= remove)
+            {
+                int count = 0;
+                while (count < remove)
+                {
+                    stack.Pop();
+                    count++;
+                }
+            }
+
+        }
+
+        static void AddStack(Stack<int> stack, string[] current)
+        {
+            stack.Push(int.Parse(current[1]));
+            stack.Push(int.Parse(current[2]));
         }
     }
 }
