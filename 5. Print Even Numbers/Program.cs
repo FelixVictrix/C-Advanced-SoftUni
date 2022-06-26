405f5ccf9be5c7c22365556b748382ced0d8cfdb
FelixVictrix <93392968+FelixVictrix@users.noreply.github.com>
2022-06-26 13:33:03 +0300

    0

---
+++ 5. Print Even Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5._Print_Even_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            array = array.Where(x => x % 2 == 0).ToArray();
+            Queue<int> queue = new Queue<int>(array);
+
+            Console.WriteLine(string.Join(", ", queue));
+
+        }
+    }
+}
