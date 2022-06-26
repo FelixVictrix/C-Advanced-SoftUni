ff767c0df916c7919175ca91d9ddb6ef90adac77
FelixVictrix <93392968+FelixVictrix@users.noreply.github.com>
2022-06-26 13:16:08 +0300

    0

---
+++ 4. Matching Brackets/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _4._Matching_Brackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string input = Console.ReadLine();
+            Stack<int> index = new Stack<int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                string item = input[i].ToString();
+                if (item == "(")
+                {
+                    int currIndex = i;
+                    index.Push(currIndex);
+                }
+                else if (item == ")")
+                {
+                    int startIndex = index.Pop();
+                    int endIndex = i;
+                    string text = input.Substring(startIndex, endIndex - startIndex + 1);
+                    Console.WriteLine(text);
+                }
+            }
+        }
+    }
+}
