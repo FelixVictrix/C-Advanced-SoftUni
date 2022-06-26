3c3a8445e303cb62856e1f9b9009116c71c2ed89
FelixVictrix <93392968+FelixVictrix@users.noreply.github.com>
2022-06-26 13:48:12 +0300

    0

---
+++ 6. Supermarket/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _6._Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> names = new Queue<string>();
+
+            string input = Console.ReadLine();
+
+            while (input != "End")
+            {
+                if (input == "Paid")
+                {
+                    Console.WriteLine(string.Join("\n",names));
+                    names.Clear();
+                }
+                else
+                {
+                    names.Enqueue(input);
+                }
+                
+
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"{names.Count} people remaining.");
+        }
+    }
+}
