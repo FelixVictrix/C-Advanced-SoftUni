77a3adc81115dda8c2041e4895cbc910a2d85186
FelixVictrix <93392968+FelixVictrix@users.noreply.github.com>
2022-06-26 12:28:42 +0300

    0

---
+++ 3. Simple Calculator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3._Simple_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split().Reverse().ToArray();
+            Stack<string> stack = new Stack<string>(input);
+
+            int sum = 0;
+
+            while (stack.Count > 0)
+            {
+                string action = stack.Pop();
+
+                if (action == "+")
+                {
+                    sum += int.Parse(stack.Pop());
+                }
+                else if (action == "-")
+                {
+                    sum -= int.Parse(stack.Pop());
+                }
+                else
+                {
+                    sum = int.Parse(action);
+                }
+                
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
