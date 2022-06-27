3df1ce75e2979a819dc668cd91a4c6cd3e6d3114
FelixVictrix <93392968+FelixVictrix@users.noreply.github.com>
2022-06-27 16:14:11 +0300

    0

---
+++ 01. Basic Stack Operations/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Basic_Stack_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int toPush = array[0];
+            int toPop = array[1];
+            int toPeek = array[2];
+
+            Stack<int> stack = new Stack<int>(numbers);
+
+           
+
+            for (int i = 0; i < toPop; i++)
+            {
+                stack.Pop();
+            }
+            if (!stack.Any())
+            {
+                Console.WriteLine("0");
+                return;
+            }
+            string isTrue = stack.Contains(toPeek)
+                ? "true"
+                : $"{stack.Min()}";
+            Console.WriteLine(isTrue);
+        }
+    }
+}
