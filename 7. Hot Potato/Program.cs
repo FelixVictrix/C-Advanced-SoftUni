354538acab047ee79c52144958bf0639feeb2a49
FelixVictrix <93392968+FelixVictrix@users.noreply.github.com>
2022-06-27 15:35:09 +0300

    0

---
+++ 7. Hot Potato/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _7._Hot_Potato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var names = Console.ReadLine().Split();
+            Queue<string> queueNames = new Queue<string>(names);
+
+            int count = int.Parse(Console.ReadLine());
+
+            while (queueNames.Count > 1)
+            {
+                int currCount = 0;
+                int end = queueNames.Count;
+                Stack<string> stack = new Stack<string>();
+
+                for (int i = 0; i < end; i++)
+                {
+                    currCount++;
+                    if (currCount != count)
+                    {
+                        stack.Push(queueNames.Dequeue());
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Removed {queueNames.Dequeue()}");
+                    }
+                }
+                queueNames = new Queue<string>(stack);
+            }
+            Console.WriteLine($"Last is {queueNames.Peek()}");
+        }
+    }
+}
---
+++ 8. Traffic Jam/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _8._Traffic_Jam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            Queue<string> queueOfCars = new Queue<string>();
+
+            string input = Console.ReadLine();
+            int count = 0;
+
+            while (input != "end")
+            {
+                if (input != "green")
+                {
+                    queueOfCars.Enqueue(input);
+                }
+                else
+                {
+                    if (n > queueOfCars.Count)
+                    {
+                        n = queueOfCars.Count;
+                    }
+                    for (int i = 0; i < n; i++)
+                    {
+                        count++;
+                        Console.WriteLine($"{queueOfCars.Dequeue()} passed!");
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"{count} cars passed the crossroads.");
+        }
+    }
+}
