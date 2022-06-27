dbc167fa00115186897a80420756b42f775cdb66
FelixVictrix <93392968+FelixVictrix@users.noreply.github.com>
2022-06-27 16:39:36 +0300

    0

---
+++ 02. Basic Queue Operations/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Basic_Stack_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int toDequ = array[1];
+            int toPeek = array[2];
+
+            Queue<int> queue = new Queue<int>(numbers);
+
+
+
+            for (int i = 0; i < toDequ; i++)
+            {
+                queue.Dequeue();
+            }
+            if (!queue.Any())
+            {
+                Console.WriteLine("0");
+                return;
+            }
+            string isTrue = queue.Contains(toPeek)
+                ? "true"
+                : $"{queue.Min()}";
+            Console.WriteLine(isTrue);
+        }
+    }
+}
---
+++ 03. Maximum and Minimum Element/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Maximum_and_Minimum_Element
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                var command = input[0];
+
+                switch (command)
+                {
+                    case 1:
+                        var element = input[1];
+                        stack.Push(element);
+                        break;
+                    case 2:
+                        if (stack.Any())
+                        {
+                            stack.Pop();
+                        }
+                        break;
+                    case 3:
+                        if (stack.Any())
+                        {
+                            Console.WriteLine(stack.Max());
+                        }
+                        break;
+                    case 4:
+                        if (stack.Any())
+                        {
+                            Console.WriteLine(stack.Min());
+                        }
+                        break;
+                }
+            }
+            if (stack.Any())
+            {
+                Console.WriteLine(string.Join(", ",stack));
+            }
+        }
+    }
+}
