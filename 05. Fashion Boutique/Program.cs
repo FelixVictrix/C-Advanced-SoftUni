215a6f396b5b61d75f260d7984a3c671e02a46d8
FelixVictrix <93392968+FelixVictrix@users.noreply.github.com>
2022-06-28 18:27:48 +0300

    0

---
+++ 05. Fashion Boutique/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Fashion_Boutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var clothes = Console.ReadLine().Split().Reverse().Select(int.Parse).ToArray();
+            var rackCap = int.Parse(Console.ReadLine());
+
+            Stack<int> stack = new Stack<int>(clothes);
+
+            var racks = 1;
+            var sum = 0;
+
+            while (stack.Count > 0)
+            {
+                var first = stack.Peek();
+
+                if (sum + first > rackCap)
+                {
+                    racks++;
+                    sum = 0;
+                }
+                else
+                {
+                    sum += first;
+                    stack.Pop();
+                }
+            }
+            Console.WriteLine(racks);
+        }
+    }
+}
---
+++ 06. Songs Queue/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06._Songs_Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var songs = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+            Queue<string> queueSongs = new Queue<string>();
+
+            foreach (var item in songs)
+            {
+                queueSongs.Enqueue(item);
+            }
+
+            while (queueSongs.Count > 0)
+            {
+
+                var input = Console.ReadLine().Split();
+                var command = input[0];
+
+                switch (command)
+                {
+                    case "Play":
+                        queueSongs.Dequeue();
+                        break;
+                    case "Add":
+                        var name = input.c
+                        var contains = queueSongs.Contains(name);
+                        if (!contains)
+                        {
+                            queueSongs.Enqueue(name);
+                            continue;
+                        }
+                        Console.WriteLine($"{name} is already contained!");
+                        break;
+                    case "Show":
+                        Console.WriteLine(string.Join(", ", queueSongs));
+                        break;
+                }
+
+            }
+            Console.WriteLine("No more songs!");
+        }
+    }
+}
