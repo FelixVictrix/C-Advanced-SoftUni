2a8045f36708393ddaa2445a93d2198f0408a0b2
FelixVictrix <93392968+FelixVictrix@users.noreply.github.com>
2022-06-29 19:09:15 +0300

    0

--- 07. Truck Tour/Program.cs
+++ 07. Truck Tour/Program.cs
@@ -8,7 +8,7 @@ namespace _07._Truck_Tour
     {
         static void Main(string[] args)
         {
-           
+
             int n = int.Parse(Console.ReadLine());
 
             Queue<int[]> queue = new Queue<int[]>();
@@ -19,6 +19,37 @@ namespace _07._Truck_Tour
                 queue.Enqueue(input);
             }
 
+            var startIndex = 0;
+           
+            while (true)
+            {
+                var totalLiters = 0;
+                var isComplete = true;
+
+                foreach (int[] item in queue)
+                {
+                    var liters = item[0];
+                    var distnace = item[1];
+
+                    totalLiters += liters;
+
+                    if (totalLiters - distnace < 0)
+                    {
+                        startIndex++;
+                        int[] currPump = queue.Dequeue();
+                        queue.Enqueue(currPump);
+                        isComplete = false;
+                        break;
+                    }
+                    totalLiters-=distnace;
+                }
+
+                if (isComplete)
+                {
+                    Console.WriteLine(startIndex);
+                    break;
+                }
+            }
         }
     }
 }
---
+++ 08. Balanced Parenthesis/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08._Balanced_Parenthesis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            Stack<char> stack = new Stack<char>();
+
+            foreach (var item in input)
+            {             
+                if (item == '{' || item == '[' || item == '(')
+                {
+                    stack.Push(item);
+                }
+                else if (true)
+                {
+
+                }
+            }
+
+        }
+    }
+}
