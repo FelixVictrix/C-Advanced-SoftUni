f4a81092e1fead3f40f16bbd69353a631f77789d
FelixVictrix <93392968+FelixVictrix@users.noreply.github.com>
2022-06-29 20:35:33 +0300

    0

--- 08. Balanced Parenthesis/Program.cs
+++ 08. Balanced Parenthesis/Program.cs
@@ -10,19 +10,46 @@ namespace _08._Balanced_Parenthesis
         {
             var input = Console.ReadLine();
             Stack<char> stack = new Stack<char>();
+            bool isBalanced = true;
 
             foreach (var item in input)
-            {             
+            {
                 if (item == '{' || item == '[' || item == '(')
                 {
                     stack.Push(item);
+                    continue;
                 }
-                else if (true)
+                if (stack.Count == 0)
                 {
-
+                    isBalanced = false;
+                    break;
+                }
+                if (item == '}' && stack.Peek() == '{')
+                {
+                    stack.Pop();
+                }
+                else if (item == ']' && stack.Peek() == '[')
+                {
+                    stack.Pop();
+                }
+                else if (item == ')' && stack.Peek() == '(')
+                {
+                    stack.Pop();
+                }
+                else
+                {
+                    isBalanced = false;
+                    break;
                 }
             }
-
+            if (!isBalanced || stack.Count > 0)
+            {
+                Console.WriteLine("NO");
+            }
+            else
+            {
+                Console.WriteLine("YES");
+            }
         }
     }
 }
