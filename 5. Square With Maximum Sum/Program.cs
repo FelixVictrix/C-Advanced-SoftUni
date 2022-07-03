973ba1c6a36e514e15b7518cc384253234eef91a
FelixVictrix <93392968+FelixVictrix@users.noreply.github.com>
2022-07-03 16:19:52 +0300

    0

--- 5. Square With Maximum Sum/Program.cs
+++ 5. Square With Maximum Sum/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _5._Square_With_Maximum_Sum
 {
@@ -6,7 +7,60 @@ namespace _5._Square_With_Maximum_Sum
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var input = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries)
+               .Select(int.Parse)
+               .ToArray();
+
+            var rows = input[0];
+            var cols = input[1];
+            int[,] matrix = new int[rows, cols];
+            AddMatrix(matrix);
+
+            var maxSum = int.MinValue;
+            var topRow = 0;
+            var topCol = 0;
+
+            for (int row = 0; row < matrix.GetLength(0) - 1; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 1; col++)
+                {
+                    var sum = matrix[row, col] + matrix[row + 1, col] + matrix[row, col + 1] + matrix[row + 1, col + 1];
+
+                    if (maxSum < sum)
+                    {
+                        maxSum = sum;
+                        topRow = row;
+                        topCol = col;
+                    }
+                }
+            }
+            for (int row = topRow; row < topRow + 2; row++)
+            {
+                for (int col = topCol; col < topCol + 2; col++)
+                {
+                    Console.Write(matrix[row,col] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine(maxSum);
+        }
+
+        static void AddMatrix(int[,] matrix)
+        {
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                var current = Console.ReadLine()
+                    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = current[col];
+                }
+            }
         }
     }
 }
