2fcdf9aa23c1cd4adf1ff28a0bcfa4e3e8fe0a06
FelixVictrix <93392968+FelixVictrix@users.noreply.github.com>
2022-07-04 19:43:36 +0300

    0

---
+++ 4. Matrix Shuffling/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Linq;
+
+namespace _4._Matrix_Shuffling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] dimensions = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            string[,] matrix = AddMatrix(dimensions);
+
+            string input = Console.ReadLine();
+
+            while (input != "END")
+            {
+
+                string[] command = input.Split();
+                if (command.Length != 5)
+                {
+                    Console.WriteLine("Invalid input!");
+                    input = Console.ReadLine();
+                    continue;
+
+                }
+                string action = command[0];
+                int row1 = int.Parse(command[1]);
+                int col1 = int.Parse(command[2]);
+                int row2 = int.Parse(command[3]);
+                int col2 = int.Parse(command[4]);
+
+                if (row1 >= 0 && row2 >= 0 && col1 >= 0 && col2 >= 0 &&
+                    row1 < matrix.GetLength(0) && row2 < matrix.GetLength(0) &&
+                    col1 < matrix.GetLength(1) && col2 < matrix.GetLength(1) &&
+                    action == "swap")
+                {
+                    string toSwap = matrix[row1, col1];
+                    matrix[row1, col1] = matrix[row2, col2];
+                    matrix[row2, col2] = toSwap;
+                    PrintMatrix(matrix);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input!");
+                }
+
+
+                input = Console.ReadLine();
+            }
+
+        }
+
+        static void PrintMatrix(string[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write($"{matrix[row,col]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static string[,] AddMatrix(int[] dimensions)
+        {
+            int rows = dimensions[0];
+            int cols = dimensions[1];
+
+            string[,] matrix = new string[rows, cols];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                    
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = input[col];
+                }
+            }
+
+            return matrix;
+        }
+
+    }
+}
