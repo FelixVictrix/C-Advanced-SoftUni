1067dce8464996f987706bb7443d36f3e25b11d8
FelixVictrix <93392968+FelixVictrix@users.noreply.github.com>
2022-07-07 20:36:09 +0300

    0

---
+++ 05. Cities by Continent and Country/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05._Cities_by_Continent_and_Country
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = int.Parse(Console.ReadLine());
+            Dictionary<string, Dictionary<string, List<string>>> cities = new Dictionary<string, Dictionary<string, List<string>>>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                var input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                var continent = input[0];
+                var country = input[1];
+                var city = input[2];
+
+                if (!cities.ContainsKey(continent))
+                {
+                    cities.Add(continent, new Dictionary<string, List<string>>());
+                    
+                }
+                if (!cities[continent].ContainsKey(country))
+                {
+                    cities[continent].Add(country, new List<string>());
+                }
+                
+                cities[continent][country].Add(city);
+
+            }
+
+            foreach (var continent in cities)
+            {
+                Console.WriteLine($"{continent.Key}:");
+
+                foreach (var item in continent.Value)
+                {
+                    Console.WriteLine($"  {item.Key} -> {string.Join(", ", item.Value)}");
+                }
+            }
+        }
+    }
+}
