fab1a921d5ef922bb4682d86617cbb4c0d0399da
FelixVictrix <93392968+FelixVictrix@users.noreply.github.com>
2022-07-07 20:09:00 +0300

    0

---
+++ 03. Largest 3 Numbers/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Largest_3_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var list = Console.ReadLine().Split().Select(int.Parse).OrderByDescending(x => x).Take(3).ToList();
+
+            Console.WriteLine(string.Join(" ", list));
+        }
+    }
+}
---
+++ 04. Product Shop/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Product_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, double>> shops = new Dictionary<string, Dictionary<string, double>>();
+
+            var input = Console.ReadLine();
+
+            while (input != "Revision")
+            {
+                var shopInfo = input.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+                var shopName = shopInfo[0];
+                var product = shopInfo[1];
+                var price = double.Parse(shopInfo[2]);
+
+                if (!shops.ContainsKey(shopName))
+                {
+                    shops.Add(shopName, new Dictionary<string, double>());
+                }
+                shops[shopName].Add(product, price);
+
+                input = Console.ReadLine();
+            }
+
+            shops = shops.OrderBy(s => s.Key).ToDictionary(x => x.Key, x => x.Value);
+
+            foreach (var shop in shops)
+            {
+                Console.WriteLine($"{shop.Key}->");
+
+                foreach (var item in shop.Value)
+                {
+                    Console.WriteLine($"Product: {item.Key}, Price: {item.Value}");
+                }
+            }
+        }
+    }
+}
