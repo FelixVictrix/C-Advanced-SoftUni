34f94ee182fd95d68c515814b82ec2812ab26b3f
FelixVictrix <93392968+FelixVictrix@users.noreply.github.com>
2022-07-07 19:44:10 +0300

    0

--- 02. Average Student Grades/Program.cs
+++ 02. Average Student Grades/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _02._Average_Student_Grades
 {
@@ -6,7 +8,35 @@ namespace _02._Average_Student_Grades
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            var lines = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<decimal>> students = new Dictionary<string, List<decimal>>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                var input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                var name = input[0];
+                var grade = decimal.Parse(input[1]);
+
+                if (!students.ContainsKey(name))
+                {
+                    students.Add(name, new List<decimal>());
+                }
+                students[name].Add(grade);
+            }
+
+            foreach (var student in students)
+            {
+                //Console.WriteLine($"{student.Key} -> {string.Join(" ",student.Value):f2} (avg: {student.Value.Average():f2})");
+                Console.Write($"{student.Key} -> ");
+                foreach (var grade in student.Value)
+                {
+                    Console.Write($"{grade:f2} ");
+                }
+                Console.WriteLine($"(avg: {student.Value.Average():f2})");
+            }
+
         }
     }
 }
