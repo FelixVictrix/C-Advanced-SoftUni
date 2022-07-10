117f4a1e2d62afaf6b4ef7e0cc1ca3822bd21bcf
FelixVictrix <93392968+FelixVictrix@users.noreply.github.com>
2022-07-10 14:28:44 +0300

    0

---
+++ 06. Record Unique Names/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06._Record_Unique_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var lines = int.Parse(Console.ReadLine());
+            HashSet<string> names = new HashSet<string>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                var input = Console.ReadLine();
+                names.Add(input);
+            }
+            Console.WriteLine(string.Join("\n", names));
+        }
+    }
+}
---
+++ 07. Parking Lot/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._Parking_Lot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            HashSet<string> cars = new HashSet<string>();
+
+            var input = Console.ReadLine();
+
+            while (input != "END")
+            {
+
+                var command = input.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+                var action = command[0];
+                var car = command[1];
+
+                switch (action)
+                {
+                    case "IN":
+                        cars.Add(car);
+                        break;
+                    case "OUT":
+                        cars.Remove(car);
+                        break;
+                }
+                input = Console.ReadLine();
+            }
+            if (cars.Any())
+            {
+                Console.WriteLine(string.Join("\n", cars));
+            }
+            else
+            {
+                Console.WriteLine(" Parking Lot is Empty");
+            }
+            
+        }
+    }
+}
