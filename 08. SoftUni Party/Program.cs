418f07782ab885ce14b4adcd897234c31c299d6f
FelixVictrix <93392968+FelixVictrix@users.noreply.github.com>
2022-07-11 13:14:21 +0300

    0

---
+++ 08. SoftUni Party/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08._SoftUni_Party
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            HashSet<string> guestsVip = new HashSet<string>();
+            HashSet<string> guests = new HashSet<string>();
+
+            var input = Console.ReadLine();
+
+            while (input != "PARTY")
+            {
+                if (input.Length>8)
+                {
+                    input = Console.ReadLine();
+                    continue;
+                }
+                if (char.IsDigit(input[0]))
+                {
+                    guestsVip.Add(input);
+                }
+                else
+                {
+                    guests.Add(input);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                if (input.Length > 8)
+                {
+                    input = Console.ReadLine();
+                    continue;
+                }
+
+                var contains = guestsVip.Contains(input)
+                    ? guestsVip.Remove(input)
+                    : guests.Remove(input);
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(guests.Count + guestsVip.Count);
+            //Console.WriteLine(string.Join("\n", guestsVip));
+            //Console.WriteLine(string.Join("\n",guests));
+            foreach (var item in guestsVip)
+            {
+                Console.WriteLine(item);
+            }
+            foreach (var item in guests)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
--- test/Program.cs
+++ test/Program.cs
@@ -10,28 +10,6 @@ namespace test
         static void Main(string[] args)
         {
 
-            SortedDictionary<int, string> studentsNumber = new SortedDictionary<int, string>(new NumbersComparer());
-
-            studentsNumber.Add(13, "Plamen");
-            studentsNumber.Add(5, "Tino");
-            studentsNumber.Add(20, "Santi");
-            studentsNumber.Add(16, "Santin");
-            studentsNumber.Add(2, "Santino");
-
-            foreach (var studentNum in studentsNumber)
-            {
-                Console.WriteLine($"{studentNum.Key} -> {studentNum.Value}");
-            }
-
-        }
-        class NumbersComparer : IComparer<int>
-        {
-            public int Compare([AllowNull] int x, [AllowNull] int y)
-            {
-                // return y - x;
-                return y % 2 == 0 ? 1 : -1;
-            }
-
         }
     }
 }
