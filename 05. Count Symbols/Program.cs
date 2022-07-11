4088557fbd1dc7968eaafaef83a8043b3a77128f
FelixVictrix <93392968+FelixVictrix@users.noreply.github.com>
2022-07-11 19:09:18 +0300

    0

---
+++ 05. Count Symbols/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Count_Symbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var text = Console.ReadLine().ToCharArray();
+            SortedDictionary<char, int> pairs = new SortedDictionary<char, int>();
+
+            foreach (var item in text)
+            {
+                if (!pairs.ContainsKey(item))
+                {
+                    pairs.Add(item, 0);
+                }
+                pairs[item]++;
+            }
+            foreach (var kvp in pairs)
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value} time/s");
+            }
+        }
+    }
+}
