71760102f83121414dbc0ef3763393112b109664
FelixVictrix <93392968+FelixVictrix@users.noreply.github.com>
2022-07-16 16:24:15 +0300

    0

---
+++ 07. Predicate For Names/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace _07._Predicate_For_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int lenght = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine().Split();
+
+            Func<int,int,bool> isLenght = (x,y) 
+                => x <= y;
+
+           names = names.Where(x=> isLenght(x.Length,lenght)).ToArray();
+            Console.WriteLine(string.Join(Environment.NewLine,names));
+
+        }
+    }
+}
