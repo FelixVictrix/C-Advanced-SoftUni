8eab06a1a27edc9285f36e84747650dd69238025
FelixVictrix <93392968+FelixVictrix@users.noreply.github.com>
2022-08-02 18:24:30 +0300

    0

---
+++ GenericSwapMethodIntegers/Box.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace GenericSwapMethodIntegers
+{
+   public class Box<T>
+    {
+        private List<T> list = new List<T>();
+
+        public void Add(T input)
+        {
+            list.Add(input);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in list)
+            {
+                sb.AppendLine($"{typeof(T)}: {item}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+
+        public void Swap(int indexOut , int indexIn)
+        {
+            T current = list[indexOut];
+            list[indexOut] = list[indexIn];
+            list[indexIn] = current;
+        }
+    }
+}
---
+++ GenericSwapMethodIntegers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace GenericSwapMethodIntegers
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var lenght = int.Parse(Console.ReadLine());
+            var box = new Box<int>();
+
+            for (int i = 0; i < lenght; i++)
+            {
+                var currInput = int.Parse(Console.ReadLine());
+                box.Add(currInput);
+            }
+
+            var input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            box.Swap(input[0], input[1]);
+            Console.WriteLine(box.ToString());
+        }
+    }
+}
