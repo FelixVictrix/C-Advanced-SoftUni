ceaeab8e03471694cf63d40f1178423cd99931ec
FelixVictrix <93392968+FelixVictrix@users.noreply.github.com>
2022-08-12 16:46:46 +0300

    0

---
+++ GenericCountMethodDouble/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GenericCountMethodDouble
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var n = int.Parse(Console.ReadLine());
+            var box = new Box<double>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = double.Parse(Console.ReadLine());
+                box.Add(input);
+            }
+            var elements = double.Parse(Console.ReadLine());
+            var count = box.Count(elements);
+            Console.WriteLine(count);
+        }
+    }
+}
