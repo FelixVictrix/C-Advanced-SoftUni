980585632b6c13fc8e256b39069bfea6414d0c29
FelixVictrix <93392968+FelixVictrix@users.noreply.github.com>
2023-05-07 17:01:25 +0300

    0

--- 003. Maximum and Minimum Element/Program.cs
+++ 003. Maximum and Minimum Element/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _003._Maximum_and_Minimum_Element
 {
@@ -6,7 +8,41 @@ namespace _003._Maximum_and_Minimum_Element
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Stack<int> stack = new Stack<int>();
+
+            int queries = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < queries; i++)
+            {
+                int[] input = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                int query = input[0];
+
+                switch (query)
+                {
+                    case 1:
+                        stack.Push(input[1]); break;
+                    case 2:
+                        if (stack.Any())
+                            stack.Pop(); break;
+                    case 3:
+                        if (stack.Any())
+                        {
+                            Console.WriteLine(stack.Max());
+                        }
+                        break;
+                    case 4:
+                        if (stack.Any()) Console.WriteLine(stack.Min());
+                        break;
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", stack));
+
         }
     }
 }
