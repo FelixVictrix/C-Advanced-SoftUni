7f8751739ba69c081a8ab6c808d7fa90a807fdda
FelixVictrix <93392968+FelixVictrix@users.noreply.github.com>
2023-05-07 17:39:20 +0300

    0

---
+++ 004. Fast Food/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _004._Fast_Food
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var foodQuantity = int.Parse(Console.ReadLine());
+
+            var ordersQuantity = Console.ReadLine()
+                .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> ordersQueue = new Queue<int>(ordersQuantity);
+
+            var biggestOrder = ordersQueue.Max();
+            Console.WriteLine(biggestOrder);
+
+            while (ordersQueue.Any())
+            {
+                var currentOrderQuantity = ordersQueue.Peek();
+
+                if (currentOrderQuantity > foodQuantity)
+                {
+                    Console.WriteLine($"Orders left: {string.Join(" ", ordersQueue)}");
+                    return;
+                }
+
+                foodQuantity -= ordersQueue.Dequeue();
+            }
+
+            Console.WriteLine("Orders complete");
+
+        }
+    }
+}
