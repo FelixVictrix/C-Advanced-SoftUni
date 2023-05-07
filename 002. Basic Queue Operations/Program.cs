ca0f85fdbf3086b4e6a80ae8fa7a68b659c7f0c8
FelixVictrix <93392968+FelixVictrix@users.noreply.github.com>
2023-05-07 16:30:39 +0300

    0

--- 002. Basic Queue Operations/Program.cs
+++ 002. Basic Queue Operations/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _002._Basic_Queue_Operations
 {
@@ -6,7 +8,52 @@ namespace _002._Basic_Queue_Operations
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            int[] values = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int toEnqueue = values[0];
+            int toDequeue = values[1];
+            int toPeek = values[2];
+
+            Queue<int> queue = new Queue<int>(numbers);
+
+            DequeueElements(queue, toDequeue);
+            Console.WriteLine(PeekElement(queue, toPeek));
+
+        }
+
+        static string PeekElement(Queue<int> queue, int toPeek)
+        {
+            string isTrue = string.Empty;
+
+            if (!queue.Any())
+            {
+                isTrue = "0";
+            }
+            else
+            {
+                isTrue = queue.Contains(toPeek)
+                    ? "true"
+                    : queue.Min().ToString();
+            }
+            
+            return isTrue;
+        }
+
+        static void DequeueElements(Queue<int> queue, int toDequeue)
+        {
+            for (int i = 0; i < toDequeue; i++)
+            {
+                queue.Dequeue();
+            }
         }
     }
 }
