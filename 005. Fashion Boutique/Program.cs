beb837776f729a4c1c4daf390d0d9a6c55176b8f
FelixVictrix <93392968+FelixVictrix@users.noreply.github.com>
2023-05-08 17:51:20 +0300

    0

---
+++ 005. Fashion Boutique/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _005._Fashion_Boutique
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] clothesVolme = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int rackCapacity = int.Parse(Console.ReadLine());
+
+            Stack<int> stackOfClothes = new Stack<int>(clothesVolme);
+
+            int racksCount = 0;
+            int clothesSum = 0;
+
+            while (stackOfClothes.Any())
+            {
+                int currentClothe = stackOfClothes.Peek();
+
+                if (clothesSum + currentClothe > rackCapacity)
+                {
+                    racksCount++;
+                    clothesSum = 0;
+                    continue;
+                }
+
+                if (stackOfClothes.Count == 1)
+                {
+                    racksCount++;
+                    break;
+                }
+
+                stackOfClothes.Pop();
+                clothesSum += currentClothe;
+            }
+
+            Console.WriteLine(racksCount);
+        }
+    }
+}
