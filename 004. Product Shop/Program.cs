0734e97010107fab5e50009c9d459166591d1145
FelixVictrix <93392968+FelixVictrix@users.noreply.github.com>
2023-07-29 19:55:57 +0300

    #

---
+++ 004. Product Shop/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _004._Product_Shop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Dictionary<string,Dictionary<string,double>> shopsList = new Dictionary<string, Dictionary<string, double>>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "Revision")
+                {
+                    break;
+                }
+
+                string[] shopInfo = input.Split(", ");
+
+                string shop = shopInfo[0];
+                string product = shopInfo[1];
+                double price = double.Parse(shopInfo[2]);
+
+                if (!shopsList.ContainsKey(shop))
+                {
+                    shopsList.Add(shop, new Dictionary<string,double>());
+                }
+                shopsList[shop].Add(product, price);
+
+            }
+
+            foreach (var kvp in shopsList.OrderBy(x=> x.Key))
+            {
+                Console.WriteLine($"{kvp.Key}->");
+                foreach (var shop in kvp.Value)
+                {
+                    Console.WriteLine($"Product: {shop.Key}, Price: {shop.Value}");
+                }
+            }
+
+        }
+    }
+}
