4c4de266c3e902d349b966b090369e9db7f65ece
FelixVictrix <93392968+FelixVictrix@users.noreply.github.com>
2023-07-30 18:14:45 +0300

    #

---
+++ 005. Cities by Continent and Country/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _005._Cities_by_Continent_and_Country
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int count = int.Parse(Console.ReadLine());
+
+            Dictionary<string, Dictionary<string, List<string>>> continents =
+                new Dictionary<string, Dictionary<string, List<string>>>();
+
+            AddToContinents(count, continents);
+            Print(continents);
+
+        }
+
+        static void Print(Dictionary<string, Dictionary<string, List<string>>> continents)
+        {
+
+            foreach (var continent in continents)
+            {
+                Console.WriteLine($"{continent.Key}:");
+
+                foreach (var info in continent.Value)
+                {
+                    Console.WriteLine($"  {info.Key} -> {string.Join(", ", info.Value)}");
+                }
+            }
+
+        }
+
+        static void AddToContinents(int count, Dictionary<string, Dictionary<string, List<string>>> continents)
+        {
+
+            for (int i = 0; i < count; i++)
+            {
+
+                string[] input = Console.ReadLine().Split(' ');
+
+                string continent = input[0];
+                string country = input[1];
+                string city = input[2];
+
+                if (!continents.ContainsKey(continent))
+                {
+                    continents.Add(continent, new Dictionary<string, List<string>> { });
+                }
+
+                if (!continents[continent].ContainsKey(country))
+                {
+                    continents[continent].Add(country, new List<string>());
+                }
+
+                continents[continent][country].Add(city);
+
+            }
+
+
+        }
+    }
+}
