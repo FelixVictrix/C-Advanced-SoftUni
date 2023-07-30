dc4bfd41255569ba58189ace6d689a7686172bd8
FelixVictrix <93392968+FelixVictrix@users.noreply.github.com>
2023-07-30 17:38:59 +0300

    #

---
+++ 004cs. Product Shop/Products.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Principal;
+
+namespace _004cs._Product_Shop
+{
+    class Products
+    {
+
+        public string Product { get; set; }
+        public double Price { get; set; }
+ 
+    }
+}
---
+++ 004cs. Product Shop/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _004cs._Product_Shop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<string, List<Products>> shopsList = new Dictionary<string, List<Products>>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "Revision")
+                {
+                    break;
+                }
+
+                string[] shopInfo = input
+                    .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+                AddToShopsList(shopInfo, shopsList);
+
+            }
+
+            Console.WriteLine();
+            PrintShopsInfo(shopsList);
+        }
+
+        static void PrintShopsInfo(Dictionary<string, List<Products>> shopsList)
+        {
+            foreach (var shop in shopsList.OrderBy(x=> x.Key))
+            {
+                Console.WriteLine($"{shop.Key}->");
+                foreach (var items in shop.Value)
+                {
+                    Console.WriteLine($"Product: {items.Product}, Price: {items.Price}");
+                }
+            }
+        }
+
+        static void AddToShopsList(string[] shopInfo, Dictionary<string, List<Products>> shopsList)
+        {
+            string shopName = shopInfo[0];
+            Products currentItem = new Products();
+            currentItem.Product = shopInfo[1];
+            currentItem.Price = double.Parse(shopInfo[2]);
+
+            if (!shopsList.ContainsKey(shopName))
+            {
+                shopsList.Add(shopName, new List<Products>());
+            }
+            
+            shopsList[shopName].Add(currentItem);
+            
+        }
+    }
+}
